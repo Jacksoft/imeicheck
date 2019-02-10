a985b5b79c1f7e8cf90db0244838fc2f40690984
Jacksoft <jacksoft87@gmail.com>
2019-02-10 21:32:42 +0100

    First Version

---
+++ IMEIChecksum/MainForm.Designer.cs
@@ -0,0 +1,111 @@
+﻿namespace IMEIChecksum {
+    partial class MainForm {
+        /// <summary>
+        /// Variabile di progettazione necessaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Pulire le risorse in uso.
+        /// </summary>
+        /// <param name="disposing">ha valore true se le risorse gestite devono essere eliminate, false in caso contrario.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Codice generato da Progettazione Windows Form
+
+        /// <summary>
+        /// Metodo necessario per il supporto della finestra di progettazione. Non modificare
+        /// il contenuto del metodo con l'editor di codice.
+        /// </summary>
+        private void InitializeComponent() {
+            this.IMEIBox = new System.Windows.Forms.TextBox();
+            this.IMEIButton = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.IMEILabel = new System.Windows.Forms.Label();
+            this.BlogLinkLabel = new System.Windows.Forms.LinkLabel();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // IMEIBox
+            // 
+            this.IMEIBox.Location = new System.Drawing.Point(6, 19);
+            this.IMEIBox.Name = "IMEIBox";
+            this.IMEIBox.Size = new System.Drawing.Size(158, 20);
+            this.IMEIBox.TabIndex = 0;
+            // 
+            // IMEIButton
+            // 
+            this.IMEIButton.Location = new System.Drawing.Point(170, 17);
+            this.IMEIButton.Name = "IMEIButton";
+            this.IMEIButton.Size = new System.Drawing.Size(75, 23);
+            this.IMEIButton.TabIndex = 1;
+            this.IMEIButton.Text = "Check + Fix";
+            this.IMEIButton.UseVisualStyleBackColor = true;
+            this.IMEIButton.Click += new System.EventHandler(this.IMEIButton_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.IMEILabel);
+            this.groupBox1.Controls.Add(this.IMEIBox);
+            this.groupBox1.Controls.Add(this.IMEIButton);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(253, 66);
+            this.groupBox1.TabIndex = 2;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "IMEI";
+            // 
+            // IMEILabel
+            // 
+            this.IMEILabel.AutoSize = true;
+            this.IMEILabel.Location = new System.Drawing.Point(6, 42);
+            this.IMEILabel.Name = "IMEILabel";
+            this.IMEILabel.Size = new System.Drawing.Size(0, 13);
+            this.IMEILabel.TabIndex = 2;
+            // 
+            // BlogLinkLabel
+            // 
+            this.BlogLinkLabel.AutoSize = true;
+            this.BlogLinkLabel.LinkColor = System.Drawing.Color.Silver;
+            this.BlogLinkLabel.Location = new System.Drawing.Point(117, 81);
+            this.BlogLinkLabel.Name = "BlogLinkLabel";
+            this.BlogLinkLabel.Size = new System.Drawing.Size(145, 13);
+            this.BlogLinkLabel.TabIndex = 3;
+            this.BlogLinkLabel.TabStop = true;
+            this.BlogLinkLabel.Text = "https://blog.jacksoftlabs.com";
+            this.BlogLinkLabel.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.BlogLinkLabel_LinkClicked);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(274, 99);
+            this.Controls.Add(this.BlogLinkLabel);
+            this.Controls.Add(this.groupBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.Name = "MainForm";
+            this.ShowIcon = false;
+            this.Text = "IMEI Checksum Fix";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox IMEIBox;
+        private System.Windows.Forms.Button IMEIButton;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label IMEILabel;
+        private System.Windows.Forms.LinkLabel BlogLinkLabel;
+    }
+}
+
---
+++ IMEIChecksum/MainForm.cs
@@ -0,0 +1,105 @@
+﻿/* IMEI Checksum Checker + Fixer
+ * This simple tool calculate and fix the checksum of any 15-digit IMEI you'll input
+ * This could be usefull for research purposes and on how an IMEI checksum is being calculated
+ * Anyway I'll not assume any responsability about the use you'll do with this tool and any damage you could cause to your devices
+ * 
+ * Copyright (c) 2019 by Jacksoft
+ */
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace IMEIChecksum {
+    public partial class MainForm : Form {
+        public MainForm() {
+            InitializeComponent();
+        }
+
+        public void IMEICheck(string imei) {
+            // If it's 15 digit it removes the last one to recalculate the checksum
+            if (imei.Length == 15) {
+                imei = imei.Remove(14, 1);
+            }
+
+            // Initialize all variables
+            int imeipart1 = 0;
+            int imeipart2 = 0;
+            int imeisum = 0;
+            int imeichecksum = 0;
+            int digit = 0;
+
+            // First block: extract odd values and sum them
+            for (int i = 0; i < imei.Length; i++) {
+                if (i % 2 == 0) {
+                    imeipart1 += int.Parse(imei[i].ToString());
+                }
+            }
+
+            // Second block: extract pair values and shift to match the checksum table
+            // 0=>0, 1=>2, 2=>4, 3=>6, 4=>8, 5=>1, 6=>3, 7=>5, 8=>7, 9=>9
+            for (int i = 0; i < imei.Length; i++) {
+                if (i % 2 == 1) {
+                    digit = int.Parse(imei[i].ToString());
+                    if (digit >= 5) {
+                        digit = ((digit * 2) % 10) + 1;
+                    }
+                    else {
+                        digit = digit * 2;
+                    }
+                    imeipart2 += digit;
+                }
+            }
+
+            // Sum the two results
+            imeisum = imeipart1 + imeipart2;
+
+            // The checksum is the difference from the sum to the next round value
+            for (int i = 1; (imeisum % 10) != 0; i++) {
+                imeisum++;
+                imeichecksum = i;
+            }
+
+            IMEIBox.Text = imei + imeichecksum;
+            IMEIBox.ForeColor = Color.Green;
+            IMEILabel.Text = "IMEI correctly checked and fixed!";
+        }
+
+        private void IMEIButton_Click(object sender, EventArgs e) {
+            // Some checks before starting calculations
+            double n = 0;
+            bool isNumeric = double.TryParse(IMEIBox.Text, out n);
+            int imeilenght = IMEIBox.Text.Length;
+            if (imeilenght == 0) {
+                IMEILabel.Text = "Please write a valid IMEI first.";
+                IMEIBox.ForeColor = Color.Red;
+            }
+            else if (isNumeric) {
+                if (imeilenght < 14) {
+                    IMEILabel.Text = "IMEI must be 14 or 15 digits.";
+                    IMEIBox.ForeColor = Color.Red;
+                }
+                else if (imeilenght > 15) {
+                    IMEILabel.Text = "IMEI is too long. Max lenght is 15 digits.";
+                    IMEIBox.ForeColor = Color.Red;
+                }
+                else if (imeilenght == 14 || imeilenght == 15) {
+                    IMEICheck(IMEIBox.Text);
+                }
+            }
+            else {
+                IMEILabel.Text = "Please input numbers ONLY!";
+                IMEIBox.ForeColor = Color.Red;
+            }
+        }
+
+        private void BlogLinkLabel_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e) {
+            System.Diagnostics.Process.Start("https://blog.jacksoftlabs.com");
+        }
+    }
+}
